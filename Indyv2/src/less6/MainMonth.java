83e99b7fb6b0b4b94e7b782d15c95a78535396b5
sergiy <sergiy@sergiy-Lenovo-IdeaPad-Y510P>
2017-03-02 15:27:05 +0200

    h

--- Indyv2/src/less6/MainMonth.java
+++ Indyv2/src/less6/MainMonth.java
@@ -7,6 +7,9 @@ public class MainMonth {
 		for (int i = 0; i < months.length; i++) {
 			System.out.println(months[i]+" "+months[i].days+" "+months[i].season);
 		}
-		
+		//зробити те саме що з днями, але з сезонами
+		//тобто користувач вводить назву сезону
+		//а програма виводить місяці в цьому сезоні
+		//не забуваємо що користувач вводить стрічку
 	}
 }
