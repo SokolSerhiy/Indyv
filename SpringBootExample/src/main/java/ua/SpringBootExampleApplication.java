aeb394e834e6327e6af969b2ea3281af4d8e73ed
sergiy <sergiy@sergiy-Lenovo-IdeaPad-Y510P>
2017-06-13 15:20:54 +0300

    dsf

--- SpringBootExample/src/main/java/ua/SpringBootExampleApplication.java
+++ SpringBootExample/src/main/java/ua/SpringBootExampleApplication.java
@@ -2,11 +2,40 @@ package ua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import ua.entity.Author;
+import ua.entity.Book;
+import ua.repository.AuthorRepository;
+import ua.repository.BookRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(SpringBootExampleApplication.class, args);
+		AuthorRepository authorRepository = run.getBean(AuthorRepository.class);
+		BookRepository bookRepository = run.getBean(BookRepository.class);
+//		Author author = new Author();
+//		author.setName("Taras Shewchenko");
+//		authorRepository.save(author);
+//		Book book = new Book();
+//		book.setTitle("Kobzar");
+//		book.setAuthor(author);
+//		bookRepository.save(book);
+//		Book book2 = new Book();
+//		book2.setAuthor(author);
+//		book2.setTitle("Bukwar");
+//		bookRepository.save(book2);
+		Author author = authorRepository.findByName("Taras Shewchenko");
+		System.out.println(author);
+		for(Book book : author.getBooks()){
+			System.out.println(book);
+		}
+		run.close();
 	}
 }
---
+++ SpringBootExample/src/main/java/ua/entity/Author.java
@@ -0,0 +1,51 @@
+package ua.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	@OneToMany(mappedBy="author")
+	private List<Book> books = new ArrayList<>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", name=" + name + "]";
+	}
+}
